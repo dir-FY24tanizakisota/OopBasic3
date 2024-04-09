c6bbe180bcf6080b4f9b91485ee70aafb988b390
dir-FY24tanizakisota <sota.tanizaki.j728@dirbato.co.jp>
2024-04-09 14:22:36 +0900

    Lesson4終了

---
+++ OopBasic3/src/company/ObjectSample.java
@@ -0,0 +1,58 @@
+package company;
+
+public class ObjectSample {
+
+    public static void main(String[] args) {
+        //----変更（ここから）-----
+        // インスタンスの作成
+        var salesDepartment = new Department("営業部", "xx", 1000000);
+        Employee sales = new Sales("鈴木", salesDepartment, "課長", 100);
+
+        // インスタンスメソッドの呼び出し
+        sales.report();
+        sales.report(2);
+        sales.joinMeeting();
+
+        // 1行空ける
+        System.out.println("");
+
+        // インスタンスの作成
+        var devDepartment = new Department("開発部", "yy", 0);
+        var engineer = new Engineer("田中", devDepartment, "一般社員", 88, "Java");
+
+        // インスタンスメソッドの呼び出し
+        engineer.report();
+        engineer.joinMeeting();
+        engineer.developSoftware();
+
+        // 1行空ける
+        System.out.println("");
+
+        // ポリモーフィズムの確認
+        Employee projectManager = new Engineer("佐藤", devDepartment, "PM", 99, "Java");
+
+        // インスタンスメソッドの呼び出し
+        projectManager.report();
+        projectManager.joinMeeting();
+        if (projectManager instanceof Engineer) {
+            // developSoftwareメソッドを呼び出す
+            ((Engineer) projectManager).developSoftware();
+        }
+
+        // ----- 追記（ここから） -----
+        // 1行空ける
+        System.out.println("");
+
+        // アルバイトインスタンスの作成
+        var parttimeworker = new ParttimeWorker("太田", salesDepartment);
+        
+        // 実装したメソッドの呼び出し
+        ((Workable) sales).work();
+        ((Workable) engineer).work();
+        ((Workable) projectManager).work();
+        ((Workable) parttimeworker).work();
+        // ----- 追記（ここまで） -----
+
+    }
+
+}
